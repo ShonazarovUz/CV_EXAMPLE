55eab32f5fa29e0894586cc9c9eda926d551abd4
ShonazarovUz <shonazarovnasriddinuz@gmail.com>
2024-10-23 14:48:29 +0500

    Api

--- routes/api.php
+++ routes/api.php
@@ -7,18 +7,39 @@ use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\SkillController;
 use App\Http\Controllers\SocialNetworkController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/user', function (Request $request) {
 //    return $request->user();
 //})->middleware('auth:sanctum');
 
-Route::resource('/users', UserController::class)->middleware('auth:sanctum');
-Route::resource('/projects', ProjectController::class)->middleware('auth:sanctum');
-Route::resource('/educations', EducationController::class)->middleware('auth:sanctum');
-Route::resource('/experiences', ExperienceController::class)->middleware('auth:sanctum');
-Route::resource('/languages', LanguageController::class)->middleware('auth:sanctum');
-Route::resource('/skills', SkillController::class)->middleware('auth:sanctum');
-Route::resource('/social_networks', SocialNetworkController::class)->middleware('auth:sanctum');
-Route::resource('/skills', SkillController::class)->middleware('auth:sanctum');
+Route::resource('/users', UserController::class)
+    ->middleware('auth:sanctum');
+
+
+Route::resource('/projects', ProjectController::class)
+    ->middleware('auth:sanctum');
+
+
+Route::resource('/educations', EducationController::class)
+    ->middleware('auth:sanctum');
+
+
+Route::resource('/experiences', ExperienceController::class)
+    ->middleware('auth:sanctum');
+
+
+Route::resource('/languages', LanguageController::class)
+    ->middleware('auth:sanctum');
+Route::get('/languages', [LanguageController::class, 'index']);
+
+Route::resource('/skills', SkillController::class)
+    ->middleware('auth:sanctum');
+
+
+Route::resource('/social_networks', SocialNetworkController::class)
+    ->middleware('auth:sanctum');
+
+
+Route::resource('/skills', SkillController::class)
+    ->middleware('auth:sanctum');
